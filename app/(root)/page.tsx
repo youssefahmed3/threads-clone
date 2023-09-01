22827696a81b1ddf3a2b79a579d46017d7acd1cc
youssefahmed3 <78634572+youssefahmed3@users.noreply.github.com>
2023-09-01 23:21:16 +0300

    added ignore build errors for typescript

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
- import { currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+
 export default async function Home() {
   const user = await currentUser();
   const result = await fetchPosts(1, 30);
